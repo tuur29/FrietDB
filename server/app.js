074e1909071f283d32c6f3718e302d0611ea384d
Tuur Lievens <tuur.lievens@hotmail.com>
2017-11-18 21:05:52 +0100

    Allow all CORS headers 6

--- server/app.js
+++ server/app.js
@@ -46,7 +46,7 @@ process.on('SIGINT', function() {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.FRONTEND_DOMAIN);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, *");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, *");
   next();
 });
 
