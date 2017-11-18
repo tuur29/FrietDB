215509fcd32bd7336cbb3083fe009d8b52bf3215
Tuur Lievens <tuur.lievens@hotmail.com>
2017-11-18 18:27:35 +0100

    edit production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // Both locked to requests from one hostname
-  backendurl: "https://frietdb.herokuapp.com:3000/api",
+  backendurl: "https://frietdb.herokuapp.com/api",
   mapskey: 'AIzaSyAeRJXpFu1i4vyWjjrbRin4DQ6bLM2ZO2U'
 };
