09c07260e9f73526c0947cd0495d899dfa9a6ad2
Tuur Lievens <tuur.lievens@hotmail.com>
2017-11-19 00:51:21 +0100

    Improve shop page layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { LightMaterialModule } from './lightmaterial.module';
 
 import { NoConflictStyleCompatibilityMode } from '@angular/material';
-import { MatExpansionModule, MatGridListModule, MatTableModule, MatPaginatorModule, MatRippleModule,  MatSelectModule, MatSortModule
+import { MatExpansionModule, MatTableModule, MatPaginatorModule, MatRippleModule,  MatSelectModule, MatSortModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [ MatExpansionModule, MatGridListModule, MatTableModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule
+  imports: [ MatExpansionModule, MatTableModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule
   ],
   exports: [
-    LightMaterialModule, MatExpansionModule, MatGridListModule, MatTableModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule
+    LightMaterialModule, MatExpansionModule, MatTableModule, MatPaginatorModule, MatRippleModule, MatSelectModule, MatSortModule
   ],
 })
 export class MaterialModule { }
