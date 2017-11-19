b3b76a7d723d021daa2439bbc67027d18a041c9d
Tuur Lievens <tuur.lievens@hotmail.com>
2017-11-19 19:58:53 +0100

    Remove gridlist dependencies

--- src/app/lightmaterial.module.ts
+++ src/app/lightmaterial.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NoConflictStyleCompatibilityMode } from '@angular/material';
-import { MatToolbarModule, MatListModule, MatGridListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
+import { MatToolbarModule, MatListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
 } from '@angular/material';
 
 @NgModule({
   imports: [ NoConflictStyleCompatibilityMode,
-    MatToolbarModule, MatListModule, MatGridListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
+    MatToolbarModule, MatListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
   ],
   exports: [
-    MatToolbarModule, MatListModule, MatGridListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
+    MatToolbarModule, MatListModule, MatSnackBarModule, MatDialogModule, MatTooltipModule, MatIconModule, MatButtonModule, MatCardModule, MatAutocompleteModule, MatInputModule, MatProgressSpinnerModule
   ],
 })
 export class LightMaterialModule { }
