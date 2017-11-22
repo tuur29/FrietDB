44ce575f680b247f96217e7f3392ed0968f6a2f1
Tuur Lievens <tuur.lievens@hotmail.com>
2017-11-22 20:02:41 +0100

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 
-echo -e "\n\n Starting!\n"
-
 # Angular build
-read -n1 -r -p "Press key if there are no uncommited changes" key
+read -n1 -rep $'\nPress key if there are no uncommited changes\n' key
 rm -rf dist
 sed -i -e 's/\/dist/#\/dist/g' .gitignore
 ng build -prod --base-href="//frietkoten.gent/"
-read -n1 -r -p "Press key if angular app has build successfully" key
+read -n1 -rep $'\nPress key if angular app has build successfully\n' key
 cp "dist/index.html" "dist/404.html"
 echo "frietkoten.gent" > "dist/CNAME"
 
@@ -18,8 +16,8 @@ git reset HEAD~
 git checkout -- .
 
 # Heroku
-read -n1 -r -p "Press key if you want to deploy to heroku" key
+read -n1 -rep $'\nPress key if you want to deploy to heroku\n' key
 git subtree push --prefix server heroku master
 rm -rf dist
 
-echo -e "\n\n Done!\n"
+echo -e "\n\n Deployed!\n"
