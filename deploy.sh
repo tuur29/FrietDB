2b107628689da6e947fe7633994fdc403825e565
Tuur Lievens <tuur.lievens@hotmail.com>
2018-08-20 16:03:18 +0200

    Fix deploy script for static build

--- deploy.sh
+++ deploy.sh
@@ -6,18 +6,12 @@ sed -i -e 's/\/dist/#\/dist/g' .gitignore
 ng build -prod
 read -n1 -rep $'\nPress key if angular app has build successfully\n' key
 cp "dist/index.html" "dist/404.html"
-echo "frietkoten.gent" > "dist/CNAME"
 
 # Deploy to Github Pages
 git add .
 git commit -m "deploy"
-git push origin `git subtree split --prefix dist master`:gh-pages --force
+git push origin `git subtree split --prefix dist static`:gh-pages --force
 git reset HEAD~
 git checkout -- .
-
-# Heroku
-read -n1 -rep $'\nPress key if you want to deploy to heroku\n' key
-git subtree push --prefix server heroku master
-rm -rf dist
 
 echo -e "\n\n Deployed!\n"
